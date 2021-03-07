2a2276a3228b03c2ae9129ae88cbbdebdfc4d500
Mateis <54773138+mateis-rgb@users.noreply.github.com>
2021-03-07 13:35:26 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-const { ENFILE } = require('constants')
-
 try {
     const http = require('http')    
     const fs = require('fs').promises
@@ -25,42 +23,12 @@ try {
 
     const requestListener = function (req, res) {
         switch(req.url) {
-            // Authentification
-            case "/auth":
-                end("/ressources/auth/index.html", res)
-                break;
-            case "/disconnect":
-                end("/ressources/auth/disconnect.html", res)
-                break;
-            case "/profile":
-                end("/ressources/auth/profile.html")
-                break;
-
-            // Code / IDE
-            case "/ide":
-                end("/ressources/ide/index.html", res)
-                break;
-            case "/share":
-                end("/ressources/ide/share.html", res)
-                break;
-            
-            case "/settings" :
-                end("/ressources/settings.html", res)
-                break;
-
             case "/":
-                end("/ressources/index.html", res)
+                end("/index.html", res)
                 break;
 
-                
             default:
                 end(req.url, res)
-                
-                // res.writeHead(404)
-                // res.end(`Error 404: http://${host}:${port}${req.url} was not found.`)
-            
-            case "/favicon.ico":
-                end('/public/favicon.ico', res)
         }
     }
     
@@ -68,8 +36,6 @@ try {
     server.listen(port, host, () => {
         console.log(`Server is running on http://${host}:${port} \n\n`)
     })
-
-    // function route(name) {}
 }
 catch {
     if (err) throw err
